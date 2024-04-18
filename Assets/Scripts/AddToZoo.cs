631a0e3d7543b9bbc81c623c6d2205667c3b1b7b
Quirkey016 <130463629+Quirkey016@users.noreply.github.com>
2024-04-18 13:23:48 +1000

    Finished the adding of the things

--- Assets/Scripts/AddToZoo.cs
+++ Assets/Scripts/AddToZoo.cs
@@ -1,16 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class AddToZoo : MonoBehaviour
 {
     [SerializeField] int number;
     public Glublefli g;
+    public GameObject manager;
+    public StatsManeager sM;
+    public ZooManager zM;
 
+    public TextMeshProUGUI cantCatch;
+
+    private void Start()
+    {
+        sM = GameObject.Find("ManagerManager").GetComponent<StatsManeager>();
+        zM = GameObject.Find("ManagerManager").GetComponent<ZooManager>();
+    }
 
     public void OnMouseUpAsButton()
     {
-        Debug.Log(g.glublefliRarity.ToString());
+        zM = GameObject.Find("ManagerManager").GetComponent<ZooManager>();
+        if (zM.maxGloobelfub > zM._gloobelfub.Count)
+        {
+            Debug.Log(g.glublefliRarity.ToString());
+            GameObject.Find("ManagerManager").GetComponent<ZooManager>().AddGloobelfub(g.glublefliRarity);
+
+
+            Destroy(gameObject);
+        }
+
+        if (zM.maxGloobelfub <= zM._gloobelfub.Count)
+        {
+            StartCoroutine(CantCatchText());
+        }
+    }
+
+    IEnumerator CantCatchText()
+    {
+        cantCatch.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(2.5f);
+
+        cantCatch.gameObject.SetActive(false);
     }
 
 
--- Assets/Scripts/ZooManager.cs
+++ Assets/Scripts/ZooManager.cs
@@ -1,13 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ZooManager : MonoBehaviour
 {
 
-    List<GameObject> _gloobelfub = new List<GameObject>();
+    public List<GameObject> _gloobelfub = new List<GameObject>();
+    public int maxGloobelfub;
 
-    public Void AddGloobelfub()
+    #region goobelfun
+
+    public TextMeshProUGUI slotOne;
+    public TextMeshProUGUI slotTwo;
+    public TextMeshProUGUI slotThree;
+    //free slots
+
+    public bool hasFirstUpgrade = false;
+    public TextMeshProUGUI slotFour;
+    public TextMeshProUGUI slotFive;
+    //upgrade 1
+
+    public bool hasSecondUpgrade = false;
+    public TextMeshProUGUI slotSix;
+    public TextMeshProUGUI slotSeven;
+    //upgrade 2
+
+    public bool hasfinalUpgrade;
+    public TextMeshProUGUI slotEight;
+    //final upgrade
+
+    #endregion
+
+    public void Update()
+    {
+        #region free slots
+        if (_gloobelfub[0] != null)
+        {
+            slotOne.text = _gloobelfub[0].ToString();
+        }
+        if (_gloobelfub[0] == null)
+        {
+            slotOne.text = "Empty";
+        }
+        //-------------------------------
+        if (_gloobelfub[0] != null)
+        {
+            slotTwo.text = _gloobelfub[1].ToString();
+        }
+        if (_gloobelfub[0] == null)
+        {
+            slotTwo.text = "Empty";
+        }
+        //-------------------------------
+        if (_gloobelfub[0] != null)
+        {
+            slotThree.text = _gloobelfub[2].ToString();
+        }
+        if (_gloobelfub[0] == null)
+        {
+            slotThree.text = "Empty";
+        }
+        //free slots
+        #endregion
+
+        #region upgrade1
+        if (hasFirstUpgrade)
+        {
+            slotOne.text = _gloobelfub[3].ToString();
+            slotOne.text = _gloobelfub[4].ToString();
+        }
+        // UGRADE 1
+
+        #endregion
+
+        #region upgrade2
+        if (hasSecondUpgrade)
+        {
+            slotOne.text = _gloobelfub[5].ToString();
+            slotOne.text = _gloobelfub[6].ToString();
+
+        }
+        //upgrade 2
+        #endregion
+
+        #region finalupgrade
+        if (hasfinalUpgrade)
+        {
+            slotOne.text = _gloobelfub[7].ToString();
+        }
+        //final upgrade
+        #endregion
+
+
+
+
+
+    }
+
+    private void Start()
+    {
+        maxGloobelfub = 3;
+    }
+
+    public void AddGloobelfub(string rarity)
     {
 
         _gloobelfub.Add(gameObject);
